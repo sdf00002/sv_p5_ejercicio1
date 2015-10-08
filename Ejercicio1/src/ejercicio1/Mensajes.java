302d6cfc995f636f74787c5ac3404406d92ebacc
sdf00002 <sdf00002@red.ujaen.es>
2015-10-08 13:17:02 +0200

    Primera implementación de las clases Mensajes y Servicios

--- Ejercicio1/src/ejercicio1/Mensajes.java
+++ Ejercicio1/src/ejercicio1/Mensajes.java
@@ -1,17 +1,55 @@
 package ejercicio1;
 
+import java.util.Date;
+
 public class Mensajes {
 	
-	public static final String CRLF="\r\n";
+	//public static final String CRLF="\r\n";
 	public static final String OK="+OK";
 	public static final String ERR="-ERR";
-	public static final String SOL="SOL";
-	public static final String PET="PET";
-	static int ID=0;
-	static int OPERACION=1;
+	//public static final String SOL="SOL";
+	//public static final String PET="PET";
+	//static int ID=0;
+	//static int OPERACION=1;
 	static int QUIT=2;
+	protected String codigoRecibido="";
+	protected static int secuencia;
+	protected int longitud;
+	protected int secuenciaRecibida = 0;
+	protected long fecha;
+	private String mensaje = "";
+	private Servicios data = null;
 	
+	public Mensajes(Servicios s){
+		Date f=new Date();
+		fecha=f.getTime();
+		
+		//Forma cadena de texto
+		mensaje=OK+" "+secuencia+" date= "+fecha+" "+s.toString();
+		longitud=mensaje.length();
+		mensaje=mensaje+" "+longitud;
+		
+		secuencia++;
+		
+	}
 	
+	public Mensajes(String datos){
+		String [] campos=datos.split(" ");
+		if (campos.length==9){
+			codigoRecibido=campos[0];
+			secuenciaRecibida=Integer.parseInt(campos[1]);
+			fecha=Long.parseLong(campos[4]);
+			data=new Servicios(Double.parseDouble(campos[5]),Double.parseDouble(campos[6]),Double.parseDouble(campos[7]));
+		}
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
 	
 	
 
--- Ejercicio1/src/ejercicio1/Servicios.java
+++ Ejercicio1/src/ejercicio1/Servicios.java
@@ -3,6 +3,19 @@ package ejercicio1;
 
 public class Servicios {
 	
+	protected double ope1;
+	protected double ope2;
+	protected double res;
+	//protected int id=0;
+	
+	public Servicios(double op1,double op2,double result){
+		ope1=op1;
+		ope2=op2;
+		res=result;;
+	}
+	
+	
+	
 	public void autenticacion(String usu,String pass){
 			//Comprobamos si usuario y contraseña son validos
 			//Si es así
@@ -23,12 +36,52 @@ public class Servicios {
 		
 	}
 	
-	
 	public void peticion2(){
 		
 		
 	}
-		
-	
+
+
+
+	public double getOpe1() {
+		return ope1;
+	}
+
+
+
+	public void setOpe1(double ope1) {
+		this.ope1 = ope1;
+	}
+
+
+
+	public double getOpe2() {
+		return ope2;
+	}
+
+
+
+	public void setOpe2(double ope2) {
+		this.ope2 = ope2;
+	}
+
+
+
+	public double getRes() {
+		return res;
+	}
+
+
+
+	public void setRes(double res) {
+		this.res = res;
+	}
+
+
+	public String toString(){
+		String men;
+		men=String.valueOf(getOpe1())+" signoOperacion "+String.valueOf(getOpe2())+" ="+String.valueOf(getRes());
+		return men;
+	}
 
 }
